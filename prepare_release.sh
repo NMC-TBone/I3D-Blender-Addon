83e669aba16d54dff4ec904f326db3c3f3dd2e87
StjerneIdioten <StjerneIdioten@gmail.com>
2025-10-11 00:01:29 +0200

    fix(release): change release script to package without folder

--- prepare_release.sh
+++ prepare_release.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # Replace version number with new version number. Can contain dev version.
 sed -i "s/^version = \".*\"/version = \"$1\"/" $GITHUB_WORKSPACE/addon/i3dio/blender_manifest.toml
-cd $GITHUB_WORKSPACE/addon
-zip -r "i3d_exporter-$1.zip" i3dio
+cd $GITHUB_WORKSPACE/addon/i3dio
+zip -r "../i3d_exporter-$1.zip" .
