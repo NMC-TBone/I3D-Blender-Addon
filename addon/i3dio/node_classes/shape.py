5acff380b8cdb560abc5d56a0165c1e28d0a2542
Kristian <57712777+NMC-TBone@users.noreply.github.com>
2025-12-20 22:29:23 +0100

    refactor(shape): move evaluated shape and nurbscurve to evaluated.py

--- addon/i3dio/node_classes/shape.py
+++ addon/i3dio/node_classes/shape.py
@@ -1,66 +1,16 @@
-import logging
 import dataclasses
+from collections import ChainMap, OrderedDict, defaultdict
 from dataclasses import dataclass
 from typing import ClassVar
-import numpy as np
-import mathutils
-from collections import OrderedDict, ChainMap, defaultdict
+
 import bpy
+import mathutils
+import numpy as np
 
-from .node import Node, SceneGraphNode
-
-from .. import debugging, xml_i3d
+from .. import xml_i3d
 from ..i3d import I3D
-
-
-class EvaluatedMesh:
-    def __init__(self, i3d: I3D, mesh_object: bpy.types.Object, *,
-                 node: SceneGraphNode | None = None,
-                 reference_frame: mathutils.Matrix = None):
-        self.i3d = i3d
-        self.source_object = mesh_object
-        self.node = node
-        self.name: str = mesh_object.data.name
-        self.object: bpy.types.Object = None
-        self.mesh: bpy.types.Mesh = None
-        self.logger = debugging.ObjectNameAdapter(logging.getLogger(f"{__name__}.{type(self).__name__}"),
-                                                  {'object_name': self.name})
-        self.generate_evaluated_mesh(mesh_object, reference_frame)
-
-    def generate_evaluated_mesh(self, mesh_object: bpy.types.Object, reference_frame: mathutils.Matrix = None) -> None:
-        if self.i3d.get_setting('apply_modifiers'):
-            self.object = mesh_object.evaluated_get(self.i3d.depsgraph)
-            self.logger.debug("is exported with modifiers applied")
-        else:
-            self.object = mesh_object
-            self.logger.debug("is exported without modifiers applied")
-
-        self.mesh = self.object.to_mesh(preserve_all_data_layers=False, depsgraph=self.i3d.depsgraph)
-
-        # If a reference is given transform the generated mesh by that frame to place it somewhere else than center of
-        # the mesh origo
-        if reference_frame is not None:
-            self.mesh.transform(reference_frame.inverted() @ self.object.matrix_world)
-
-        conversion_matrix = self.i3d.conversion_matrix
-        if self.i3d.get_setting('apply_unit_scale'):
-            self.logger.debug("applying unit scaling")
-            conversion_matrix = \
-                mathutils.Matrix.Scale(bpy.context.scene.unit_settings.scale_length, 4) @ conversion_matrix
-
-        self.mesh.transform(conversion_matrix)
-        if conversion_matrix.is_negative:
-            self.mesh.flip_normals()
-            self.logger.debug("conversion matrix is negative, flipping normals")
-
-        # Calculates triangles from mesh polygons
-        self.mesh.calc_loop_triangles()
-
-    # On hold for the moment, it seems to be triggered at random times in the middle of an export which messes with
-    # everything. Further investigation is needed.
-    def __del__(self):
-        pass
-        # self.object.to_mesh_clear()
+from ..shapes.evaluated import EvaluatedMesh, EvaluatedNurbsCurve
+from .node import Node, SceneGraphNode
 
 
 @dataclass
@@ -801,40 +751,6 @@ class ControlVertex:
 
     def position_for_xml(self):
         return "{0:.6g} {1:.6g} {2:.6g}".format(*self._position)
-
-
-class EvaluatedNurbsCurve:
-    def __init__(self, i3d: I3D, shape_object: bpy.types.Object, name: str = None,
-                 reference_frame: mathutils.Matrix = None):
-        if name is None:
-            self.name = shape_object.data.name
-        else:
-            self.name = name
-        self.i3d = i3d
-        self.object = None
-        self.curve_data = None
-        self.logger = debugging.ObjectNameAdapter(logging.getLogger(f"{__name__}.{type(self).__name__}"),
-                                                  {'object_name': self.name})
-        self.control_vertices = []
-        self.generate_evaluated_curve(shape_object, reference_frame)
-
-    def generate_evaluated_curve(self, shape_object: bpy.types.Object, reference_frame: mathutils.Matrix = None):
-        self.object = shape_object
-
-        self.curve_data = self.object.to_curve(depsgraph=self.i3d.depsgraph)
-
-        # If a reference is given transform the generated mesh by that frame to place it somewhere else than center of
-        # the mesh origo
-        if reference_frame is not None:
-            self.curve_data.transform(reference_frame.inverted() @ self.object.matrix_world)
-
-        conversion_matrix = self.i3d.conversion_matrix
-        if self.i3d.get_setting('apply_unit_scale'):
-            self.logger.debug("applying unit scaling")
-            conversion_matrix = \
-                mathutils.Matrix.Scale(bpy.context.scene.unit_settings.scale_length, 4) @ conversion_matrix
-
-        self.curve_data.transform(conversion_matrix)
 
 
 class NurbsCurve(Node):
