143dcfc991513fbe4108d9f93e0383f67c575a0b
Kristian <57712777+NMC-TBone@users.noreply.github.com>
2025-12-31 15:47:25 +0100

    cleanup

--- addon/i3dio/ui/object.py
+++ addon/i3dio/ui/object.py
@@ -19,7 +19,7 @@ from bpy.props import (
     CollectionProperty,
 )
 from .helper_functions import i3d_property
-from ..xml_i3d import I3D_MAX as i3d_max
+from ..xml_i3d import i3d_max
 from . import (presets, mesh, light)
 
 from .collision_data import COLLISIONS
